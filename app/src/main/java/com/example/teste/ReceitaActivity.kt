88735eea23d03fa2a0b917015ef86a12091b87f6
JoaoOrtiz-Ads-b <jpsortiz@gmail.com>
2025-05-24 17:15:08 -0300

    Implementação da tela de editar receita e correção de erros

--- app/src/main/java/com/example/teste/ReceitaActivity.kt
+++ app/src/main/java/com/example/teste/ReceitaActivity.kt
@@ -1,13 +1,23 @@
-package com.diariodereceitas
+package com.example.teste
 
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
+
+import android.widget.EditText
+
 class ReceitaActivity : AppCompatActivity() {
+
+
+    private lateinit var edtReceita: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receita)
+
+
+        edtReceita = findViewById(R.id.edtReceita)
 
         val btnBack = findViewById<ImageButton>(R.id.btnBack)
         val btnEdit = findViewById<ImageButton>(R.id.btnEdit)
@@ -21,16 +31,24 @@ class ReceitaActivity : AppCompatActivity() {
 
         btnEdit.setOnClickListener {
             Toast.makeText(this, "Editar receita", Toast.LENGTH_SHORT).show()
+
         }
 
         btnShare.setOnClickListener {
             Toast.makeText(this, "Compartilhar receita", Toast.LENGTH_SHORT).show()
         }
 
-        btnConcluir.setOnClickListener {
-            Toast.makeText(this, "Receita salva!", Toast.LENGTH_SHORT).show()
-        }
-    }
-}
 
+        btnConcluir.setOnClickListener {
+            val receitaFinal = edtReceita.text.toString()
+            if (receitaFinal.isNotBlank()) {
+
+                Toast.makeText(this, "Receita salva: $receitaFinal", Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Por favor, escreva sua receita.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
